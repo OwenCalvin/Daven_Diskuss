5099a9c5809c8aea90d40a1ad36f4949240a7783
owengombas <owen.gombas@gmail.com>
2017-09-25 11:35:54 +0200

    Deconnection

--- Diskuss/Conversation.xaml.cs
+++ Diskuss/Conversation.xaml.cs
@@ -14,22 +14,27 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Diskuss {
-    public partial class Conversation : UserControl, IY {
+namespace Diskuss
+{
+    public partial class Conversation : UserControl, IY
+    {
         public UserChannelObject Object { get; set; }
         public new ConversationGrid Parent { get; set; }
         public UserChannelGrid Destination { get; set; }
         public List<Message> Messages { get; set; } = new List<Message>();
 
-        public int Y {
+        public int Y
+        {
             get { return Grid.GetRow(this); }
             set { Grid.SetRow(this, value); }
         }
 
         private int iNotifications { get; set; } = 0;
-        public int Notifications {
+        public int Notifications
+        {
             get { return iNotifications; }
-            set {
+            set
+            {
                 lblNotifications.Content = value;
                 iNotifications = value;
                 brdNotifications.Visibility = value < 1 ? Visibility.Hidden : Visibility.Visible;
@@ -37,40 +42,59 @@ namespace Diskuss {
         }
 
         private bool bDisconnected = false;
-        public bool Disconnected {
+        public bool Disconnected
+        {
             get { return bDisconnected; }
-            set {
-                lblName.Foreground = value ? Brushes.Red : Brushes.White;
+            set
+            {
+                if (value)
+                {
+                    Object.brdBack.Background = (Brush)(new BrushConverter().ConvertFrom("#2E3438"));
+                    lblName.FontStyle = FontStyles.Italic;
+                }
+                else
+                {
+                    Object.brdBack.Background = (Brush)(new BrushConverter().ConvertFrom("#485056"));
+                    lblName.FontStyle = FontStyles.Normal;
+                }
                 bDisconnected = value;
             }
         }
 
-        public Conversation() {
+        public Conversation()
+        {
             InitializeComponent();
         }
 
-        public Conversation(UserChannelObject Object) {
+        public Conversation(UserChannelObject Object)
+        {
             InitializeComponent();
             this.Object = Object;
             Destination = Object.Parent;
-            lblName.Content = Object.Name.Length > 6 ? $"{Object.Name.Substring(0, 6)}..." : Object.Name ;
+            lblName.Content = Object.Name.Length > 6 ? $"{Object.Name.Substring(0, 6)}..." : Object.Name;
             Object.lblFullName.Visibility = Visibility.Collapsed;
             grdObject.Children.Add(Object);
         }
 
-        private void Close_MouseDown(object sender, MouseButtonEventArgs e) {
+        private void Close_MouseDown(object sender, MouseButtonEventArgs e)
+        {
             Object.lblFullName.Visibility = Visibility.Visible;
             grdObject.Children.Clear();
             Parent.Remove(this);
         }
 
-        private void Grid_MouseDown(object sender, MouseButtonEventArgs e) {
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
             Parent.SelectedConversation = this;
         }
 
-        public void AddMessage(Message Message) {
-            lblLastMessage.Content = Message.Text.Length > 10 ? $"{Message.Text.Substring(0, 10)}..." : Message.Text;
-            Messages.Add(Message);
+        public void AddMessage(Message Message)
+        {
+            if (!Disconnected)
+            {
+                lblLastMessage.Content = Message.Text.Length > 10 ? $"{Message.Text.Substring(0, 10)}..." : Message.Text;
+                Messages.Add(Message);
+            }
         }
     }
 }
--- Diskuss/MainWindow.xaml.cs
+++ Diskuss/MainWindow.xaml.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
-namespace Diskuss {
-    public partial class MainWindow : Window {
+namespace Diskuss
+{
+    public partial class MainWindow : Window
+    {
         private DiskussAPI _diskuss;
 
-        public MainWindow() {
+        public MainWindow()
+        {
             InitializeComponent();
             _diskuss = new DiskussAPI(grdConversations.Children.OfType<UserChannelObject>().ToList(), grdUsersList.Children.OfType<User>().ToList(), grdChannelsList.Children.OfType<Channel>().ToList());
 
@@ -26,79 +30,102 @@ namespace Diskuss {
             _diskuss.OnSendPrivateMessage += _diskuss_OnSendPrivateMessage;
         }
 
-        private void _diskuss_OnSendPrivateMessage(object sender, Message Message) {
+        private void _diskuss_OnSendPrivateMessage(object sender, Message Message)
+        {
             grdConversations.SelectedConversation.AddMessage(Message);
             AddMessage(Message);
         }
 
-        private void _diskuss_OnNewPrivateMessage(object sender, Message Message) {
+        private void _diskuss_OnNewPrivateMessage(object sender, Message Message)
+        {
             UserChannelObject _ucUser = grdUsersList.Children.OfType<UserChannelObject>().FirstOrDefault(x => x.Name == Message.Sender);
-            if (_ucUser != null) {
+            if (_ucUser != null)
+            {
                 grdUsersList.Remove(_ucUser);
                 grdConversations.Add(_ucUser);
             }
 
             Conversation _convUser = grdConversations.GetConversation(Message.Sender);
             _convUser.AddMessage(Message);
-            if (_convUser != grdConversations.SelectedConversation) {
+            if (_convUser != grdConversations.SelectedConversation)
+            {
                 _convUser.Notifications = ++_convUser.Notifications;
-            } else {
+            }
+            else
+            {
                 AddMessage(Message);
             }
         }
 
-        private void GrdConversations_OnConversationSelectedChange(object sender, Conversation Conversation) {
+        private void GrdConversations_OnConversationSelectedChange(object sender, Conversation Conversation)
+        {
             grdChat.Children.Clear();
             grdChat.RowDefinitions.Clear();
             tbxMessage.Text = null;
-            if ((lblChatName.Content = Conversation) == null) {
+            if ((lblChatName.Content = Conversation) == null)
+            {
                 lblChatName.Content = "";
                 msgForm.Visibility = Visibility.Collapsed;
-            } else {
+            }
+            else
+            {
                 lblChatName.Content = Conversation.Object.Name.Length > 25 ? $"{Conversation.Object.Name.Substring(0, 25)}..." : Conversation.Object.Name;
                 msgForm.Visibility = Visibility.Visible;
                 Conversation.Messages.ForEach(e => { AddMessage(e); });
             }
         }
 
-        private void _diskuss_OnLogin(object sender, EventArgs e) {
+        private void _diskuss_OnLogin(object sender, EventArgs e)
+        {
             grdLogin.Visibility = Visibility.Collapsed;
             lblNick.Content = _diskuss.Me.Name.Length > 13 ? $"{_diskuss.Me.Name.Substring(0, 13)}..." : _diskuss.Me.Name;
         }
 
-        private void _diskuss_OnChannels(object sender, List<UserChannelObject> Objects) {
+        private void _diskuss_OnChannels(object sender, List<UserChannelObject> Objects)
+        {
             grdChannelsList.Objects = Objects;
         }
 
-        private void _diskuss_OnUsers(object sender, List<UserChannelObject> Objects) {
+        private void _diskuss_OnUsers(object sender, List<UserChannelObject> Objects)
+        {
             grdUsersList.Objects = Objects;
-            /*grdConversations.Conversations.ForEach(_conv => {
-                bool bDisconnected = false;
-                Objects.ForEach(_uc => {
-                    bDisconnected = _uc.Name == _conv.Object.Name;
-                });
-                _conv.Disconnected = bDisconnected;
-            });*/
+
+            grdConversations.Conversations.ForEach(_conv => {
+                bool bConnected = false;
+                foreach (UserChannelObject _uc in Objects)
+                {
+                    if (bConnected = _uc.Name == _conv.Object.Name)
+                    {
+                        break;
+                    }
+                }
+                _conv.Disconnected = !bConnected;
+            });
         }
-        
-        private void btnLogin_Click(object sender, RoutedEventArgs e) {
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
             _diskuss.Login(tbxForm.Text.Length > 0 ? tbxForm.Text : "Anonymous", true);
         }
 
-        private void btn_ChannelClick(object sender, EventArgs e) {
+        private void btn_ChannelClick(object sender, EventArgs e)
+        {
             cancelForm();
             _diskuss.JoinChannel(tbxForm.Text);
         }
 
-        private void grdAddChannel_MouseDown(object sender, MouseButtonEventArgs e) {
+        private void grdAddChannel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
             showForm("Join", btn_ChannelClick, true);
         }
 
-        private void showForm(string BtnContent, RoutedEventHandler EventClick, bool Cross) {
+        private void showForm(string BtnContent, RoutedEventHandler EventClick, bool Cross)
+        {
             grdLogin.Visibility = Visibility.Visible;
             tbxForm.Text = null;
             grdBtn.Children.Clear();
-            Button _btn = new Button() {
+            Button _btn = new Button()
+            {
                 Content = BtnContent,
                 Style = Resources["BtnYellow"] as Style,
                 FontFamily = btnLogin.FontFamily,
@@ -116,25 +143,31 @@ namespace Diskuss {
             grdBtn.Children.Add(_btn);
         }
 
-        private void Cross_MouseDown(object sender, MouseButtonEventArgs e) {
+        private void Cross_MouseDown(object sender, MouseButtonEventArgs e)
+        {
             cancelForm();
         }
 
-        private void cancelForm() {
+        private void cancelForm()
+        {
             grdLogin.Visibility = Visibility.Collapsed;
         }
 
-        private void btnSend_MouseDown(object sender, MouseButtonEventArgs e) {
+        private void btnSend_MouseDown(object sender, MouseButtonEventArgs e)
+        {
             _diskuss.SendPrivateMessage(grdConversations.SelectedConversation.Object.Name, tbxMessage.Text);
         }
 
-        private void tbxMessage_KeyDown(object sender, KeyEventArgs e) {
-            if (e.Key == Key.Enter) {
+        private void tbxMessage_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
                 _diskuss.SendPrivateMessage(grdConversations.SelectedConversation.Object.Name, tbxMessage.Text);
             }
         }
 
-        private void AddMessage(Message Message) {
+        private void AddMessage(Message Message)
+        {
             grdChat.RowDefinitions.Add(new RowDefinition() { Height = new GridLength() });
             Grid.SetRow(Message, grdChat.RowDefinitions.Count - 1);
             Grid.SetColumn(Message, Message.Me ? 1 : 0);
